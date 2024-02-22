9e4a56b4ee42c54fe028722ef17b57f1a3dc5b3d
Ayan Ospanov <ospanovayan2003@gmail.com>
2024-02-23 01:07:10 +0600

    PySpark tool used to detect most and least popular death reason for each state(updated).

--- main.py
+++ main.py
@@ -1,25 +1,38 @@
 from pyspark.sql import SparkSession
-from pyspark.sql.functions import col, count
+from pyspark.sql.types import StructType, StructField, StringType, IntegerType, DateType
+from pyspark.sql.functions import col, count, desc, asc, rank
 from pyspark.sql.window import Window
-from pyspark.sql.functions import desc, asc, rank
+schema = StructType([
+    StructField("Person", StringType(), True),
+    StructField("Action", StringType(), True),
+    StructField("Date", StringType(), True),
+    StructField("City", StringType(), True),
+    StructField("Gender", StringType(), True),
+    StructField("Age", IntegerType(), True),
+])
+
 spark = SparkSession.builder.appName("EndtermBDSpark").getOrCreate()
 path = "police_fatality_with_header.csv"
-df = spark.read.csv(path, header=True, inferSchema=True)
-#to count deaths in each state
+df = spark.read.csv(path, header=True, schema=schema)
+
 action_counts = df.groupBy("City", "Action").count()
 
 windowSpecDesc = Window.partitionBy("City").orderBy(desc("count"))
 windowSpecAsc = Window.partitionBy("City").orderBy(asc("count"))
-#making top most and less popular deaths
+
 ranked_actions_desc = action_counts.withColumn("rank_desc", rank().over(windowSpecDesc))
 ranked_actions_asc = action_counts.withColumn("rank_asc", rank().over(windowSpecAsc))
 
 #most popular death reason
-most_popular_actions = ranked_actions_desc.filter(col("rank_desc") == 1).select("City", "Action", "count").orderBy("City")
+most_popular_actions = ranked_actions_desc.filter(col("rank_desc") == 1) \
+    .select("City", "Action", "count") \
+    .orderBy(desc("count"))
 #least popular death reason
-least_popular_actions = ranked_actions_asc.filter(col("rank_asc") == 1).select("City", "Action", "count").orderBy("City")
+least_popular_actions = ranked_actions_asc.filter(col("rank_asc") == 1) \
+    .select("City", "Action", "count") \
+    .orderBy(asc("count"))
 
-print("Most Popular Actions by City:")
+print("Most Popular Actions by City (sorted by count descending):")
 most_popular_actions.show(truncate=False)
-print("Least Popular Actions by City:")
+print("Least Popular Actions by City (sorted by count ascending):")
 least_popular_actions.show(truncate=False)
